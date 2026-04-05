aaa132e6c287bb12ef8d34a73fdec9ce37676318
Almog Mizrahi <almogosh111@gmail.com>
2026-04-05 15:07:37 +0300

    add js carousel func

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // main j
 
+// active cards
 function toggleService(card) {
-    const isActive = card.classList.contains('active');
+    const isActive = card.classList.contains('active')
 
     document.querySelectorAll('.service-card')
-            .forEach(c => c.classList.remove('active'));
+            .forEach(c => c.classList.remove('active'))
 
     if (!isActive) {
-        card.classList.add('active');
+        card.classList.add('active')
     }
 }
+
+// Carousel
+function scrollCarousel(dir) {
+    const track = document.getElementById('carousel')
+    track.scrollBy({ left: dir * -280, behavior: 'smooth' })
+}
+
